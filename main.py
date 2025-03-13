bbcdcc9bcba4fe44e71d06e22138b364059db6fa
Jeslyn Lai <imlyj5@gmail.com>
2025-03-13 11:32:40 -0700

    pass the first 6 tests

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 def calculate_total_sales(sales_totals):
-    pass
+    if sales_totals == {}:
+        return 0.0
+    
+    total = 0.0
+    for key, value in sales_totals.items():
+        total += value["quantity"] * value["price"]
+    return total
+        
 
 def calculate_average_sales(sales_totals):
-    pass
+    if not sales_totals:
+        return 0.0
+    total_price = 0.0
+
+    for key, value in sales_totals.items():
+        total_price += value["quantity"] * value["price"]
+
+    return total_price/len(sales_totals)
 
 def filter_to_better_than_average_sales(sales_totals):
-    pass
+    if not sales_totals:
+        return sales_totals
+    
+    filtered ={}
+    total_price = 0
+   
+    for key, value in sales_totals.items():
+        total_price += value["quantity"] * value["price"]
+        average = total_price/len(sales_totals)
+        if value["quantity"] * value["price"] > average:
+            filtered[key]= value
+    return filtered
+
+
     
 def ninety_nine_bottles(count, beverage):
     pass
--- tests/test_iteration.py
+++ tests/test_iteration.py
@@ -25,7 +25,7 @@ def test_caculate_candy_total_returns_total_sales():
     assert total == 395.0
 
 
-@pytest.mark.skip("remove this line when working on calculate_average_sales")
+#@pytest.mark.skip("remove this line when working on calculate_average_sales")
 def test_caculate_candy_average_no_data_returns_0():
     sales_totals = {}
 
@@ -34,7 +34,7 @@ def test_caculate_candy_average_no_data_returns_0():
     assert average == 0.0
 
 
-@pytest.mark.skip("remove this line when working on calculate_average_sales")
+#@pytest.mark.skip("remove this line when working on calculate_average_sales")
 def test_caculate_candy_average_returns_average():
     sales_totals = {
         "Junior Mints": { "quantity": 100, "price": 2.0 },
@@ -48,7 +48,7 @@ def test_caculate_candy_average_returns_average():
     assert average == 98.75
 
 
-@pytest.mark.skip("remove this line when working on filter_to_better_than_average_sales")
+#@pytest.mark.skip("remove this line when working on filter_to_better_than_average_sales")
 def test_filter_to_above_average_sales_no_data_returns_empty_dict():
     sales_totals = {}
 
@@ -57,7 +57,7 @@ def test_filter_to_above_average_sales_no_data_returns_empty_dict():
     assert filtered == {}
 
 
-@pytest.mark.skip("remove this line when working on filter_to_better_than_average_sales")
+#@pytest.mark.skip("remove this line when working on filter_to_better_than_average_sales")
 def test_filter_to_above_average_sales_returns_filtered_result():
     sales_totals = {
         "Junior Mints": { "quantity": 100, "price": 2.0 },
